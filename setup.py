a80f40d10fcca71584e7385f2fd4c743e6d435df
alfred82santa <alfred82santa@gmail.com>
2016-05-02 11:25:29 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='aio-service-client',
     url='https://github.com/alfred82santa/aio-service-client',
     author='alfred82santa',
-    version='0.1.2',
+    version='0.1.3',
     author_email='alfred82santa@gmail.com',
     classifiers=[
         'Intended Audience :: Developers',
